8266e92825aac4f8da0be4552c2103995cb7dd5a
Dat Nguyen <dat.nguyen@MACs-MacBook-Pro.local>
2020-08-04 15:08:34 +0700

    add css

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 import Styles from './Dialog.module.css';
 
 function Dialog(props) {
-  const { children } = props;
+  const { children, isShowedUp, toggleDialog } = props;
 
   return (
-    <div className={Styles.wrapper}>
+    <div className={
+      classNames(
+        Styles.wrapper,
+        !isShowedUp ? Styles.hidden : ''
+        )}
+      onClick={toggleDialog}>
       <div className={Styles.contentWrapper}>
         {children}
       </div>
     </div>
   )
 }
+
+export default Dialog;
